e6ed9aa925d74f84ba9b9d7dd7ec6f7df44e0fda
Olivier de Mirleau <mirleau@bluewin.ch>
2012-11-11 18:09:35 +0100

    Simplifying command class hierarchy #31 #32.

--- src/webdebate-portlet/docroot/WEB-INF/src/com/arguments/functional/requeststate/ArgsRequest4.java
+++ src/webdebate-portlet/docroot/WEB-INF/src/com/arguments/functional/requeststate/ArgsRequest4.java
@@ -16,13 +16,15 @@ import com.arguments.functional.requeststate.PortalArgsBridge.UpdateState;
  * @author mirleau
  *
  */
-public class ArgsRequest4 extends ArgsRequest2
+public class ArgsRequest4
 {
+    private final ArgsRequest2 theAR2;
+
     // ------------------------------------------------------------------------
     public ArgsRequest4(ArgsRequest aRequest, UrlContainer aUrlContainer,
             CgiSource aStateInputMode, UpdateState aUpdateState)
     {
-        super(aRequest, aUrlContainer, aStateInputMode, aUpdateState, null);
+        theAR2 = new ArgsRequest2(aRequest, aUrlContainer, aStateInputMode, aUpdateState, null);
     }
     
     // ------------------------------------------------------------------------
@@ -30,12 +32,12 @@ public class ArgsRequest4 extends ArgsRequest2
     {
         // This method performs a state update twice. First here:
         ArgsStatefulCommand myArgsRequest =
-                TheContainerBridge.i().storeStateGetArgumentsRequest(this);
+                TheContainerBridge.i().storeStateGetArgumentsRequest(theAR2);
         // Not here, which is where you would want it
         ArgsState myState = myArgsRequest.execute();
         StateChange myStateString = myState.getStateString();
         assertTrue(myStateString.hasChange());
         // Then a second time here:
-        myStateString.mergeAndStore(getAppUser());
+        myStateString.mergeAndStore(theAR2.getAppUser());
     }
 }
